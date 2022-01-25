ebcb99ba545127abdd97527faf43cc397f639520
p22cgussen <p22cgussen@fairfieldprep.org>
2022-01-24 23:19:17 -0500

    complete endlabs

---
+++ sem2-end-labs-5.py
@@ -0,0 +1,11 @@
+# Charlie CCG 1/24/2022
+
+def is_triangle(a, b, c):
+    if ((a<b+c) and (b<a+c) and (c<a+b)) == True:
+        return True
+    else:
+        return False
+
+print(is_triangle(1, 2, 2) == True)
+print(is_triangle(7, 2, 2) == False)
+print(is_triangle(1, 2, 3) == False)
